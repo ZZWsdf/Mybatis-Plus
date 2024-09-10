c03892e2b5ad63f18b34f7ae3e3e19cc49af2cb7
ZZWsdf <1097798399@qq.com>
2024-09-10 20:44:41 +0800

    mybatis plus 快速入门

--- src/main/java/com/itheima/mp/mapper/UserMapper.java
+++ src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -1,19 +1,12 @@
 package com.itheima.mp.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.itheima.mp.domain.po.User;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-public interface UserMapper{
+public interface UserMapper extends BaseMapper<User> {
 
-    void saveUser(User user);
-
-    void deleteUser(Long id);
-
-    void updateUser(User user);
-
-    User queryUserById(@Param("id") Long id);
-
-    List<User> queryUserByIds(@Param("ids") List<Long> ids);
 }
